a665d673cfdcbf686b75bc622bb030717a591eeb
nippyclouding <nippyclouding@gmail.com>
2026-01-27 07:42:45 +0900

    front

---
+++ Front/JavaScript/section01/chapter09_조건문.js
@@ -0,0 +1,30 @@
+// 1. if 조건문
+
+let num = 10;
+
+if (num >= 10) {
+    console.log("10 이상입니다.");
+} else if (num >= 5) {
+    console.log("5 이상, 10 미만 입니다.");
+} else {
+    console.log("그 외의 값입니다.");
+}
+
+
+// 2. switch 조건문
+
+let animal = "cat";
+switch (animal) {
+    case "dog" :
+        console.log("this is dog");
+        break;
+    case "bird" :
+        console.log("this is bird");
+        break;
+    case "bear" :
+        console.log("this is bear");
+        break;
+    case "cat" :
+        console.log("this is cat");
+        break;
+}
---
+++ Front/JavaScript/section01/chapter10_반복문.js
@@ -0,0 +1,11 @@
+// 반복문
+
+for (let i = 0; i < 10; i++) {
+    if (i == 5) break;
+    console.log(i + 1);
+}
+
+for (let i = 0; i < 10; i++) {
+    if (i % 2 == 0) continue;
+    console.log(i);
+}
