5e5f0298767a861c795c63abd2aa6a83e3e58cc4
nippyclouding <wonderwall09in@naver.com>
2026-02-15 17:59:18 +0900

    chat

---
+++ Back/websocket/project2/chat-front/src/router/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "@/views/HomeView.vue"
+import MemberCreate from "@/views/MemberCreate.vue"
+import LoginPage from "@/views/LoginPage.vue"
+import MemberList from "@/views/MemberList.vue"
+import GroupChatList from "@/views/GroupChatList.vue"
+
+const routes = [
+    { path: '/', name: 'Home', component: Home },
+    { path: '/member/create', name: 'MemberCreate', component: MemberCreate },
+    { path: '/login', name: 'LoginPage', component: LoginPage },
+    { path: '/member/list', name: 'MemberList', component: MemberList },
+    { path: '/groupchatting/list', name: 'GroupChatList', component: GroupChatList }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+export default router;
